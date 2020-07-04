b51e540eca1e85345021b9312437ff1e8e146312
Dave Takahashi <dtcello@gmail.com>
2020-07-03 22:51:56 -0700

    Start example tests

--- R/utilities.R
+++ R/utilities.R
@@ -10,3 +10,24 @@ get_api <- function(host = "https://www.tatorapp.com", token = Sys.getenv("TATOR
   instance$apiClient$basePath <- host
   return(instance)
 }
+
+#' Breaks a create_*_list operation into chunks.
+#'
+#'   Example:
+#'
+#'\code{} 
+#'         created_ids = c()
+#'         for (response in chunked_create(api$CreateLocalizationList,1, localization_spec=my_long_list) {
+#'             created_ids <- c(created_ids, response.id)
+#'         }
+#' }
+#'  @param func Function to call on each chunk.
+#'  @param project Unique integer identifying a project.
+#'  @return Generator that yields a response.
+#' @export
+chunked_create <- function(FUN, project, spec_list) {
+  for (idx in range(0, length(spec_list), 500)) {
+    response <- FUN(project, spec_list[idx:(idx+500)])
+    yield(response)
+  }
+}
