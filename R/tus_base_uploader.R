b3e3e3cfac81e0f69c185a5fa727233ff557d4d6
Dave Takahashi <dtcello@gmail.com>
2020-07-11 13:20:32 -0700

    Finish tus base uploader

--- R/tus_base_uploader.R
+++ R/tus_base_uploader.R
@@ -5,6 +5,7 @@
 TusBaseUploader <- R6::R6Class(
   "TusBaseUploader",
   public = list(
+    default_headers = list("Tus-Resumable" = "1.0.0"),
     file_path = NULL,
     file_stream = NULL,
     url = NULL,
@@ -47,17 +48,98 @@ TusBaseUploader <- R6::R6Class(
       self$fingerprinter <- fingerprinter # || fingerprinter$Fingerprint()
       self$offset <- 0
       self$url <- NULL
-      # self.__init_url_and_offset(url)
+      self$InitURLAndOffset(url)
       self$chunk_size <- chunk_size
       self$retries <- retries
       self$request <- NULL
       # self._retried = 0
       self$retry_delay <- retry_delay
       self$upload_checksum <- upload_checksum
-      # self.__checksum_algorithm_name, self.__checksum_algorithm = self.CHECKSUM_ALGORITHM_PAIR
+      self$checksum_algorithm_name <- "sha1"
+      self$checksum_algorithm <- "sha1"
     },
     GetHeaders <- function() {
+      client_headers <- self$client$headers
+      return(c(self$default_headers, client_headers))
+    },
+    GetURLCreationHeaders <- function() {
+      headers <- self$GetHeaders()
+      headers <- c(headers, "upload-length" = toString(self$GetFileSize()))
+      headers <- c(headers, "upload-metadata" = paste(self$EndcodeMetadata(), collapse = ","))
+      return(headers)
+    },
+    ChecksumAlgorithm <- function() {
+      return(self$checksum_algorithm)
+    },
+    ChecksumAlgorithmName <- function() {
+      return(self$checksum_algorithm_name)
+    },
+    GetOffset <- function() {
+      resp <- HEAD(self$url, add_headers(self$GetHeaders()))
+      offset <- resp$headers["upload-offset"]
+      if (is.null(offset)) {
+        stop(paste("Could not get offset, response status code", resp$status_code))
+      }
+      return(strtoi(offset))
+    },
+    EncodeMetadata <- function() {
+      encoded_list <- list()
+      for (key in names(self$metadata)) {
+        key_str <- toString(key)
+        
+        if (str_detect(key_str, regex("^$|[\s,]+"))) {
+          stop(paste("upload-metadata key", key_str, "cannot be empty or contain spaces or commas."))
+        }
+        
+        value_bytes <- iconv(self$metadata[key], to = "latin1")
+        encoded_list <- c(encoded_list, key_str = iconv(jsonlite::base64_enc(value_bytes), to = "ascii"))
+      }
+      return(encoded_list)
+    },
+    InitURLAndOffset <- function(url = NULL) {
+      if (!is.null(url)) {
+        self$SetURL(url)
+      }
       
+      if (self$store_url && !is.null(self$url_storage)) {
+        key <- self$fingerprinter$GetFingerPrint(self$GetFileStream())
+        self$SetURL(self$url_storage[key])
+      }
+      
+      if (!is.null(self$url)) {
+        self$offset <- self$GetOffset()
+      }
+    },
+    SetURL <- function(url) {
+      self$url <- url
+      
+      if (self$store_url && !is.null(self$url_storage)) {
+        key <- self$fingerprinter$GetFingerPrint(self$GetFileStream())
+        self$SetURL(self$url_storage[key])
+      }
+    },
+    GetRequestLength <- function() {
+      remainder <- self$stop_at - self$offset
+      if (remainder > self$chunk_size) {
+        return(self$chunk_size)
+      } else {
+        return(remainder)
+      }
+    },
+    GetFileStream <- function() {
+      if (!is.null(self$file_stream)) {
+        seek(self$file_stream, where = 0, origin = "start")
+        return(self$file_stream)
+      }
+      
+      if (file.exists(self$file_path)) {
+        return(open(file(self$file_path)))
+      }
+      
+      stop(paste("invalid file", self$file_path))
+    },
+    GetFileSize <- function() {
+      return(file.info(self$file_path)$size)
     }
   )
 )
