281f951574e8f0406dd01df9cbbc3a4c96465f1d
David Takahashi <dtcello@gmail.com>
2020-08-23 15:47:54 -0700

    Comment out video frames test

--- tests/testthat/test_example_video_frames.R
+++ tests/testthat/test_example_video_frames.R
@@ -1,10 +1,10 @@
 test_that("video frames", {
-  cmd <- paste(
-    "Rscript ../../examples/video_frames.R",
-    "--host", host,
-    "--token", token,
-    "--video_id", video_id,
-    sep = " ")
-  exit_code <- system(cmd)
+  # cmd <- paste(
+  #   "Rscript ../../examples/video_frames.R",
+  #   "--host", host,
+  #   "--token", token,
+  #   "--video_id", video_id,
+  #   sep = " ")
+  # exit_code <- system(cmd)
   # expect_equal(exit_code, 0)
 })
