d7752cb6f2af89bdb2ee367d7a44a8ffb59aa150
David Takahashi <dtcello@gmail.com>
2020-09-04 15:05:58 -0700

    Media test

---
+++ tests/testthat/test_media.R
@@ -0,0 +1,18 @@
+test_that("Get file", {
+  tator_api <- get_api(host, token)
+  video_obj <- tator_api$GetMedia(video_id)
+  
+  tempfile <- tempfile()
+  out_path <- paste0(dirname(tempfile), "/video.mp4")
+  download_media(tator_api, video_obj, out_path)
+  expect_true(file.exists(out_path))
+})
+
+test_that("Get audio", {
+  tator_api <- get_api(host, token)
+  video_obj <- tator_api$GetMedia(video_id)
+  
+  audio <- video_obj$media_files$audio
+  expect_true(length(audio) > 0)
+  expect_equal(audio[[3]]$codec, "aac")
+})
